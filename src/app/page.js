d63ad848b6574b9ab3ae5f4c9e4b28442be3fc03
llaenv <lillaenv@gmail.com>
2023-08-04 14:42:14 +0200

    scroll bug fixed

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ import { strings } from "@/strings/strings";
 
 export default function Home() {
 	return (
-		<main className='w-full flex flex-col min-h-screen bg-layout overflow-x-hidden '>
-			<div className='snap-container w-screen h-full lg:h-screen scroll-smooth lg:snap-y lg:snap-mandatory lg:overflow-y-scroll relative z-10'>
+		<main className='w-full flex flex-col min-h-screen bg-layout lg:overflow-x-hidden'>
+			<div className='snap-container lg:w-screen h-full lg:h-screen scroll-smooth lg:snap-y lg:snap-mandatory lg:overflow-y-scroll lg:relative lg:z-10'>
 				<Hero strings={strings.hero} />
 				<Latest strings={strings.latest_projects} />
 				<Projects strings={strings.projects} />
