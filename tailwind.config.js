1600b8f8483e42edb380a237dd45ecb1578afbb0
llaenv <lillaenv@gmail.com>
2023-08-07 22:01:04 +0200

    changed mobile bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
 				layout: "#1f1d1c",
 			},
 			backgroundImage: {
-				mobile: "url('/assets/images/backgrounds/bg-tablet.png')",
+				mobile: "url('/assets/images/backgrounds/bg-xs.png')",
 				tablet: "url('/assets/images/backgrounds/bg-tablet.png')",
 				large: "url('/assets/images/backgrounds/bg-lg.png')",
 				desktop: "url('/assets/images/backgrounds/frame4.png')",
