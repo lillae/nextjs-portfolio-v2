a3fc8f8fb9b730a6b6560f16e91bf2625432a774
llaenv <lillaenv@gmail.com>
2023-08-07 20:31:51 +0200

    hero title

--- src/components/hero/hero-component.js
+++ src/components/hero/hero-component.js
@@ -4,8 +4,8 @@ const Hero = ({ strings }) => {
 	const { title, subtitle, btn } = strings;
 
 	return (
-		<section id='home' className='snap-start h-screen  lg:h-screen'>
-			<div className='wrapper h-full flex flex-col justify-center items-start space-y-10'>
+		<section id='home' className='snap-start h-[calc(100vh+80px)] lg:h-screen'>
+			<div className='wrapper h-full flex flex-col justify-center items-center lg:items-start space-y-10'>
 				<div className='pb-16'>
 					<TitleOne>{title}</TitleOne>
 					<h2 className='text-xl  1xs:text-3xl md:text-40 leading-normal uppercase -mt-5 md:-mt-8'>
