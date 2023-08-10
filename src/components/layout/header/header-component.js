5ea487e468dc37cd596101b2c123f9d2268eadc2
llaenv <lillaenv@gmail.com>
2023-08-10 08:27:30 +0200

    desktop bg

--- src/components/layout/header/header-component.js
+++ src/components/layout/header/header-component.js
@@ -8,24 +8,23 @@ import NavMobile from "./nav-mobile-component";
 const Header = ({ strings }) => {
 	const [scrollPos, setScrollPos] = useState(0);
 
-	const handleScroll = () => {
+	const handleScrollSnap = () => {
 		const wrapper = document.querySelector(".snap-container");
 		setScrollPos(wrapper.scrollTop);
 	};
 
 	useEffect(() => {
 		const wrapper = document.querySelector(".snap-container");
-		wrapper.addEventListener("scroll", handleScroll);
-		return () => wrapper.removeEventListener("scroll", handleScroll);
+		wrapper.addEventListener("scroll", handleScrollSnap);
+		return () => wrapper.removeEventListener("scroll", handleScrollSnap);
 	}, []);
 
 	return (
 		<header
 			className={`${
-				scrollPos > 0
-					? "1xl:hidden"
-					: "bg-layout/60 backdrop-blur-md  1xl:bg-transparent 1xl:backdrop-blur-none w-full h-20 fixed bottom-0 md:top-0 left-0 z-50"
-			}`}>
+				scrollPos > 0 ? " bg-layout/10 1xl:hidden" : " "
+			} backdrop-blur-md 1xl:backdrop-blur-none w-full h-20 fixed bottom-0 md:top-0 left-0 z-50 $
+			`}>
 			<NavDesktop strings={strings} />
 			<NavMobile nav={strings.nav_mobile} />
 		</header>
